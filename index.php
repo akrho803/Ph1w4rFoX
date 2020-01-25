9ddff093cdb6f6d8ecfd9ced3bf101c4ea34ee3a
akrho803 <60284289+akrho803@users.noreply.github.com>
2020-01-25 22:00:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+	<title>Hacked By Ph1w4rFoX</title>
+	<style type="text/css">
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	body {
+		background: #000;
+		color: #0f0;
+		padding: 24px;
+	}
+	p {
+		padding: 12px 0;
+	}
+	.blink{animation:blink 1s infinite}@keyframes blink{to{opacity:.0;}}
+	</style>
+</head>
+<body style="">
+<img src="http://www.downvids.net/video/faceimage/img-700035736.jpg">
+<h1>Hacked By Ph1w4rFoX<span class="blink">_</span></h1>
+<p>Hi Admin!!</p>
+<pre>
+
+Ph1w4rFoX was Here!!!
+	Don't worry ADMIN, Your DATABASE is SAFE! Nothing deleted.
+	PATCH your SYSTEM otherwise we will be back soon! 
+<span class="blink">_</span>
+
+</pre>
+</body></html>
